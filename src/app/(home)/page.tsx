bf1e7220263c87cf10ba95f1d3cd52479b01ae01
Austin Karren <austin.t.karren@gmail.com>
2026-01-08 17:53:20 -0700

    Update page.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -57,12 +57,12 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Building robust systems with a designer’s precision.
+            {`Building robust systems with a designer’s precision.`}
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Austin, a software engineer based in Utah and the former
+            {`I’m Austin, a software engineer based in Utah and the former
             technical co-founder of Event Penguin. If I can help you build
-            something awesome, I'd love to connect!
+            something awesome, I'd love to connect!`}
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
